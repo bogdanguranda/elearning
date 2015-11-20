9c2316c10ffa610f7f6e4e35081daa012ffa80de
cuvidk <cuvidk@yahoo.ro>
2015-11-20 17:34:03 +0200

    Token-request initial commit -creates DAO and Service Exceptions

---
+++ src/main/java/thecerealkillers/elearning/exceptions/DAOException.java
@@ -0,0 +1,10 @@
+package thecerealkillers.elearning.exceptions;
+
+/**
+ * Created by cuvidk on 11/20/2015.
+ */
+public class DAOException extends Exception {
+    public DAOException(String message) {
+        super(message);
+    }
+}
