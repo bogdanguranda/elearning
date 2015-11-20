17ec010325e5ea61df529262d4d58885651a1bdc
cuvidk <cuvidk@yahoo.ro>
2015-11-21 00:33:18 +0200

    Token-request finished -every request  available on backend except sign-up and authentication now require the token passed in the header -this is done using sessionService.getSessionByToken which will result in a SessionException if it's not found in the DB => HttpStatus.NOT_FOUND -also backend team, take note that I added DAOException and ServiceException in the project, please use them on your work. Check UserDAO, Service and Controller to see how to use them for your classes. -i'm wasted. very much. sorry for late. done

--- src/main/java/thecerealkillers/elearning/controller/impl/MessageControllerImpl.java
+++ src/main/java/thecerealkillers/elearning/controller/impl/MessageControllerImpl.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import thecerealkillers.elearning.controller.MessagesController;
+import thecerealkillers.elearning.exceptions.ServiceException;
 import thecerealkillers.elearning.model.Message;
 import thecerealkillers.elearning.service.MessageService;
+import thecerealkillers.elearning.service.SessionService;
 
 import java.util.List;
 
@@ -19,40 +21,70 @@ public class MessageControllerImpl implements MessagesController {
 
     @Autowired
     private MessageService messageService;
+    @Autowired
+    private SessionService sessionService;
 
     @RequestMapping(value = "/messages", method = RequestMethod.POST)
-    public ResponseEntity createMessage(@RequestBody Message message) {
-        messageService.add(message);
+    public ResponseEntity createMessage(@RequestBody Message message, @RequestHeader(value="token") String token) {
+        try {
+            sessionService.getSessionByToken(token);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+            messageService.add(message);
+            return new ResponseEntity(HttpStatus.CREATED);
+        } catch (ServiceException service_exception) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value = "/messages", method = RequestMethod.GET)
-    public ResponseEntity<List<Message>> getAllMessages() {
-        List<Message> messageList = messageService.getAll();
+    public ResponseEntity<List<Message>> getAllMessages(@RequestHeader(value="token") String token) {
+        try {
+            sessionService.getSessionByToken(token);
 
-        return new ResponseEntity<>(messageList, HttpStatus.OK);
+            List<Message> messageList = messageService.getAll();
+            return new ResponseEntity<>(messageList, HttpStatus.OK);
+        } catch (ServiceException service_exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value = "/messages/{senderUsername}/{receiverUsername}", method = RequestMethod.GET)
-    public ResponseEntity<List<Message>> getMessagesBetweenUsers(
-            @PathVariable("senderUsername") String senderUsername, @PathVariable("receiverUsername") String receiverUsername) {
-        List<Message> messagesBetweenUsers = messageService.getMessagesBetweenUsers(senderUsername, receiverUsername);
+    public ResponseEntity<List<Message>> getMessagesBetweenUsers(@PathVariable("senderUsername") String senderUsername,
+                                                                 @PathVariable("receiverUsername") String receiverUsername,
+                                                                 @RequestHeader(value="token") String token) {
+        try {
+            sessionService.getSessionByToken(token);
 
-        return new ResponseEntity<>(messagesBetweenUsers, HttpStatus.ACCEPTED);
+            List<Message> messagesBetweenUsers = messageService.getMessagesBetweenUsers(senderUsername, receiverUsername);
+            return new ResponseEntity<>(messagesBetweenUsers, HttpStatus.ACCEPTED);
+        } catch (ServiceException service_exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value = "/messages/{username}", method = RequestMethod.GET)
-    public ResponseEntity<List<Message>> getMessagesByUser(@PathVariable("username") String username) {
-        List<Message> messagesByUser = messageService.getMessagesByUser(username);
+    public ResponseEntity<List<Message>> getMessagesByUser(@PathVariable("username") String username,
+                                                           @RequestHeader(value="token") String token) {
+        try {
+            sessionService.getSessionByToken(token);
 
-        return new ResponseEntity<>(messagesByUser, HttpStatus.ACCEPTED);
+            List<Message> messagesByUser = messageService.getMessagesByUser(username);
+            return new ResponseEntity<>(messagesByUser, HttpStatus.ACCEPTED);
+        } catch (ServiceException service_exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @RequestMapping(value = "/messages", method = RequestMethod.DELETE)
-    public ResponseEntity deleteUserMessages(@RequestParam(value = "username", required = true) String username) {
-        messageService.delete(username);
+    public ResponseEntity deleteUserMessages(@RequestParam(value = "username", required = true) String username,
+                                             @RequestHeader(value="token") String token) {
+        try {
+            sessionService.getSessionByToken(token);
 
-        return new ResponseEntity(HttpStatus.OK);
+            messageService.delete(username);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (ServiceException service_exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
