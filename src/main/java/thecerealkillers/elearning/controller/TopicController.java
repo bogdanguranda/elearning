4ef947d1c6825b1b99be4b68216dbe5d34b879a3
dani-i <iliesidaniel1@gmail.com>
2015-11-21 02:05:50 +0200

    CRUD Topic v 2015.11.21.0
    	- also moved PasswordInfo from *utilities* to *model*

--- src/main/java/thecerealkillers/elearning/controller/TopicController.java
+++ src/main/java/thecerealkillers/elearning/controller/TopicController.java
@@ -1,4 +1,24 @@
 package thecerealkillers.elearning.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import thecerealkillers.elearning.model.Topic;
+
+import java.util.List;
+
 public interface TopicController {
+    @RequestMapping(value = "/topics", method = RequestMethod.POST)
+    ResponseEntity createTopic(@RequestBody Topic newTopic);
+
+    @RequestMapping(value = "/topics", method = RequestMethod.GET)
+    ResponseEntity<List<Topic>> getAllTopics();
+
+    @RequestMapping(value = "/topics/{title}", method = RequestMethod.GET)
+    ResponseEntity<Topic> getTopicByTitle(@PathVariable("title") String title);
+
+    @RequestMapping(value = "/topics/{title}", method = RequestMethod.POST)
+    ResponseEntity updateTopic( @PathVariable ("title") String title, @RequestBody Topic newTopic);
+
+    @RequestMapping(value = "/topics", method = RequestMethod.DELETE)
+    ResponseEntity deleteTopicByTitle(@RequestParam(value = "title", required = true) String title);
 }
--- src/main/java/thecerealkillers/elearning/dao/impl/TopicDAOImpl.java
+++ src/main/java/thecerealkillers/elearning/dao/impl/TopicDAOImpl.java
@@ -1,6 +1,94 @@
 package thecerealkillers.elearning.dao.impl;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 import thecerealkillers.elearning.dao.TopicDAO;
+import thecerealkillers.elearning.model.Topic;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class TopicDAOImpl implements TopicDAO {
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void add(Topic newTopic) {
+        String command = "INSERT INTO topic VALUES (:title)";
+
+        Map<String, String> namedParameters = new HashMap<>();
+        namedParameters.put("title", newTopic.getTitle());
+
+        namedParameterJdbcTemplate.update(command, namedParameters);
+    }
+
+    @Override
+    public Topic get(String title) {
+        Map<String, String> namedParameters = Collections.singletonMap("title", title);
+        String command = "SELECT * FROM topic WHERE title = :title";
+
+        List<Topic> topicList = namedParameterJdbcTemplate.query(command, namedParameters, new RowMapper<Topic>() {
+            @Override
+            public Topic mapRow(ResultSet resultSet, int i) throws SQLException {
+                Topic topic = new Topic();
+
+                topic.setTitle(resultSet.getString("title"));
+
+                return topic;
+            }
+        });
+
+        return topicList.get(0);
+    }
+
+    @Override
+    public List<Topic> getAll() {
+        String command = "SELECT * FROM topic";
+        List<Topic> topicList;
+
+        topicList = namedParameterJdbcTemplate.query(command, new RowMapper<Topic>() {
+            @Override
+            public Topic mapRow(ResultSet resultSet, int i) throws SQLException {
+                Topic topic = new Topic();
+
+                topic.setTitle(resultSet.getString("title"));
+
+                return topic;
+            }
+        });
+
+        return topicList;
+    }
+
+    @Override
+    public void update(String title, Topic newTopicData) {
+        String command = "UPDATE topic SET title = :newTitle WHERE title = :title";
+
+        Map<String, String> namedParameters = new HashMap<>();
+        namedParameters.put("newTitle", newTopicData.getTitle());
+        namedParameters.put("title", title);
+
+        namedParameterJdbcTemplate.update(command, namedParameters);
+    }
+
+    @Override
+    public void delete(String title) {
+        String command = "DELETE FROM topic WHERE title = :title";
+        Map<String, String> namedParameters = Collections.singletonMap("title", title);
+
+        namedParameterJdbcTemplate.update(command, namedParameters);
+    }
 }
