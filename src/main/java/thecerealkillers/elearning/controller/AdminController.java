9d97d7a334293a2d3da3cbf73d6278b551964679
dani-i <iliesidaniel1@gmail.com>
2015-12-21 23:03:37 +0200

    Fix2 AdminController

--- src/main/java/thecerealkillers/elearning/controller/AdminController.java
+++ src/main/java/thecerealkillers/elearning/controller/AdminController.java
@@ -1,14 +1,11 @@
 package thecerealkillers.elearning.controller;
 
 
+import org.springframework.web.bind.annotation.*;
 import thecerealkillers.elearning.model.AccountSuspensionInfo;
 import thecerealkillers.elearning.model.ChangeAccountTypeInfo;
 import thecerealkillers.elearning.model.UserSignUpInfo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -20,20 +17,20 @@ import java.util.List;
 public interface AdminController {
 
     @RequestMapping(value = "/admin/createAccount", method = RequestMethod.POST)
-    ResponseEntity createAccount(@RequestBody UserSignUpInfo newUser, String userRole);
+    ResponseEntity createAccount(@RequestBody UserSignUpInfo newUser, String userRole, @RequestHeader(value="token") String token);
 
     @RequestMapping(value = "/admin/suspendAccount", method = RequestMethod.POST)
-    ResponseEntity suspendAccount(@RequestBody AccountSuspensionInfo suspendInfo);
+    ResponseEntity suspendAccount(@RequestBody AccountSuspensionInfo suspendInfo, @RequestHeader(value="token") String token);
 
     @RequestMapping(value = "/admin/reactivateAccount", method = RequestMethod.POST)
-    ResponseEntity reactivateAccount(@RequestBody AccountSuspensionInfo reactivateInfo);
+    ResponseEntity reactivateAccount(@RequestBody AccountSuspensionInfo reactivateInfo, @RequestHeader(value="token") String token);
 
     @RequestMapping(value = "/admin/changeAccountType", method = RequestMethod.POST)
-    ResponseEntity changeAccountType(@RequestBody ChangeAccountTypeInfo accountTypeInfo);
+    ResponseEntity changeAccountType(@RequestBody ChangeAccountTypeInfo accountTypeInfo, @RequestHeader(value="token") String token);
 
     @RequestMapping(value = "/admin/getAudit", method = RequestMethod.GET)
-    ResponseEntity<List<String>> getAudit();
+    ResponseEntity<List<String>> getAudit(@RequestHeader(value="token") String token);
 
     @RequestMapping(value = "/admin/getAuditForUser/{username}", method = RequestMethod.GET)
-    ResponseEntity<List<String>> getAuditForUser(@PathVariable("username") String username);
+    ResponseEntity<List<String>> getAuditForUser(@PathVariable("username") String username, @RequestHeader(value="token") String token);
 }
--- src/main/java/thecerealkillers/elearning/controller/impl/AdminControllerImpl.java
+++ src/main/java/thecerealkillers/elearning/controller/impl/AdminControllerImpl.java
@@ -6,10 +6,7 @@ import thecerealkillers.elearning.model.AccountSuspensionInfo;
 import thecerealkillers.elearning.model.ChangeAccountTypeInfo;
 import thecerealkillers.elearning.model.UserSignUpInfo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -22,33 +19,34 @@ import java.util.List;
 @CrossOrigin
 public class AdminControllerImpl implements AdminController {
 
+
     @Override
-    public ResponseEntity createAccount(@RequestBody UserSignUpInfo newUser, String userRole) {
+    public ResponseEntity createAccount(@RequestBody UserSignUpInfo newUser, String userRole, @RequestHeader(value = "token") String token) {
         return null;
     }
 
     @Override
-    public ResponseEntity suspendAccount(@RequestBody AccountSuspensionInfo suspendInfo) {
+    public ResponseEntity suspendAccount(@RequestBody AccountSuspensionInfo suspendInfo, @RequestHeader(value = "token") String token) {
         return null;
     }
 
     @Override
-    public ResponseEntity reactivateAccount(@RequestBody AccountSuspensionInfo reactivateInfo) {
+    public ResponseEntity reactivateAccount(@RequestBody AccountSuspensionInfo reactivateInfo, @RequestHeader(value = "token") String token) {
         return null;
     }
 
     @Override
-    public ResponseEntity changeAccountType(@RequestBody ChangeAccountTypeInfo accountTypeInfo) {
+    public ResponseEntity changeAccountType(@RequestBody ChangeAccountTypeInfo accountTypeInfo, @RequestHeader(value = "token") String token) {
         return null;
     }
 
     @Override
-    public ResponseEntity<List<String>> getAudit() {
+    public ResponseEntity<List<String>> getAudit(@RequestHeader(value = "token") String token) {
         return null;
     }
 
     @Override
-    public ResponseEntity<List<String>> getAuditForUser(@PathVariable("username") String username) {
+    public ResponseEntity<List<String>> getAuditForUser(@PathVariable("username") String username, @RequestHeader(value = "token") String token) {
         return null;
     }
 }
